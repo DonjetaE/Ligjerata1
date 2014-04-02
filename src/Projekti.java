7c1113262e7000470f7a3aafa965e941ea3c24dc
DonjetaE <donjetaelshani@gmail.com>
2014-04-02 19:41:26 +0200

    Ushtrimet e para

--- src/Projekti.java
+++ src/Projekti.java
@@ -1,9 +1,9 @@
 
 public class Projekti {
-	public static void main (String[] args)
-	{	
-		System.out.println("Projekti jone i pare");
+	public static void main (String[] args) {	
 		
+
+		System.out.println("Projekti jone i pare");
 	}
 
 }
